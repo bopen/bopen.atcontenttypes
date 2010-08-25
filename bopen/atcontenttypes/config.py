9eb99e61b00ddea86e3eca716f7955d3315aed8c
alessio@BOPEN.EU <alessio@BOPEN.EU@9cc78506-ce66-4620-b983-9dd9faf58894>
2010-08-25 09:31:48 +0000

    new: removed richpage
    git-svn-id: https://services.bopen.eu/svn/bopen/bopen.atcontenttypes/trunk@1108 9cc78506-ce66-4620-b983-9dd9faf58894

--- bopen/atcontenttypes/config.py
+++ bopen/atcontenttypes/config.py
@@ -5,6 +5,5 @@ PROJECTNAME = 'bopen.atcontenttypes'
 
 ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
-    'RichPage': 'bopen.atcontenttypes: Add RichPage',
     'RichFolder': 'bopen.atcontenttypes: Add RichFolder',
 }
