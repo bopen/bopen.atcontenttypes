f1afe05d2f05a566dfe81fa5e207c15feb92816d
Alessandro Amici <a.amici@bopen.it>
2010-09-07 18:18:59 +0200

    new: reformat and start adding requires

--- setup.py
+++ setup.py
@@ -14,57 +14,45 @@ version = '0.1'
 long_description = (
     read('README.txt')
     + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
     'Detailed Documentation\n'
     '**********************\n'
     + '\n' +
     read('bopen', 'atcontenttypes', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n')
+)
 
 tests_require = ['zope.testing']
 
-setup(name='bopen.atcontenttypes',
-      version=version,
-      description="B-Open AT Content Types",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='bopen.atcontenttypes',
+    version=version,
+    description="B-Open AT Content Types for Plone",
+    long_description=long_description,
+    classifiers=[
         'Framework :: Plone',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['bopen', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='bopen.atcontenttypes.tests.test_docs.test_suite',
-      entry_points="""
-      # -*- entry_points -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+    keywords='',
+    author='B-Open Solutions srl',
+    author_email='',
+    url='http://www.bopen.eu/open-source/bopen.atcontenttypes',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['bopen', 'bopen.atcontenttypes'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.i18nmessageid',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite='bopen.atcontenttypes.tests.test_docs.test_suite',
+    entry_points="""
+    # -*- entry_points -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins=["ZopeSkel"],
+)
